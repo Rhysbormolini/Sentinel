242057ddfcaf188985ad459c2500a03583ac5266
Rhysbormolini <rhysbormolini@gmail.com>
2021-10-22 22:19:21 +0800

    Added electric ball ability

--- Sentinel Titans War/Assets/Sentinel/Scripts/ProjectileTrigger.cs
+++ Sentinel Titans War/Assets/Sentinel/Scripts/ProjectileTrigger.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class ProjectileTrigger : MonoBehaviour
 {
 
-    public GameObject m_Projectile;    // this is a reference to your projectile prefab
-    public Transform m_SpawnTransform; // this is a reference to the transform where the prefab will spawn
+    public GameObject tornadoProjectile;    // this is a reference to your projectile prefab
+    public Transform tornadoSpawnTransform; // this is a reference to the transform where the prefab will spawn
 
     [SerializeField]
-    float abilityCoolDownTime;
-    float abilityCoolDown;
+    float tornadoAbilityCoolDownTime;
+    float tornadoAbilityCoolDown;
+
+    public GameObject lightningBallProjectile;    // this is a reference to your projectile prefab
+    public Transform lightningBallSpawnTransform; // this is a reference to the transform where the prefab will spawn
+
+    [SerializeField]
+    float lightningBallAbilityCoolDownTime;
+    float lightningBallAbilityCoolDown;
+
+    //public Animator m_Animator;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q) && abilityCoolDown <= 0)
+        if (Input.GetKeyDown(KeyCode.Q) && tornadoAbilityCoolDown <= 0)
         {
-            Instantiate(m_Projectile, m_SpawnTransform.position, m_SpawnTransform.rotation);
-            abilityCoolDown = abilityCoolDownTime;
+            //m_Animator.Play("TornadoAnim");
+            Instantiate(tornadoProjectile, tornadoSpawnTransform.position, tornadoSpawnTransform.rotation);
+            tornadoAbilityCoolDown = tornadoAbilityCoolDownTime;
         }
 
-        if (abilityCoolDown > 0)
+        if (tornadoAbilityCoolDown > 0)
         {
-            abilityCoolDown -= Time.deltaTime;
+            tornadoAbilityCoolDown -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && lightningBallAbilityCoolDown <= 0)
+        {
+            Instantiate(lightningBallProjectile, lightningBallSpawnTransform.position, lightningBallSpawnTransform.rotation);
+            lightningBallAbilityCoolDown = lightningBallAbilityCoolDownTime;
+        }
+
+        if (lightningBallAbilityCoolDown > 0)
+        {
+            lightningBallAbilityCoolDown -= Time.deltaTime;
         }
     }
 }
