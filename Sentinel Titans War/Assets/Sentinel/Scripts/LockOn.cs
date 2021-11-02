0d86a1ba4d00f61158e39c7db5c9318c392b10ee
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2021-11-03 00:46:12 +0800

    Lock on continued

--- Sentinel Titans War/Assets/Sentinel/Scripts/LockOn.cs
+++ Sentinel Titans War/Assets/Sentinel/Scripts/LockOn.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using System.Linq;
 
 namespace Gamekit3D
 {
     public class LockOn : MonoBehaviour
     {
         public float playerVisionRadius = 20f;
-        public List<Damageable> targets = new List<Damageable> { };
+        public List<GameObject> targets = new List<GameObject> { };
         public Transform nearestEnemy;
+		public Transform currentEnemy;
+		bool isLockingOn = false;
+		[SerializeField] LayerMask enemyLayer;
 
 
 		private void FixedUpdate()
@@ -16,12 +21,45 @@ namespace Gamekit3D
 			if (Input.GetKeyDown(KeyCode.Tab))
 			{
 				// swap lock on to closet target within range
+				isLockingOn = true;
+				HandleLockOn();
 			}
 
 			if (Input.GetKeyDown(KeyCode.LeftControl))
 			{
-				// swap lock on to next closet target within range 
+				// swap lock on to next closest target within range
+				SwapTargets();
 			}
+		}
+
+		public void HandleLockOn()
+		{
+			RaycastHit hit;
+			targets = Physics.SphereCast(transform.position, playerVisionRadius, transform.forward, out hit);
+		
+			
+			
+			nearestEnemy = targets
+			.OrderBy(t => Vector3.Distance(transform.position, t.transform.position)
+			.FirstOrDefault();
+		}
+
+		public void SwapTargets()
+		{
+			if (isLockingOn == true)
+			{
+				
+			}	
+		}
+
+		public void CameraLock()
+		{
+
+		}
+
+		private void OnDrawGizmos()
+		{
+			// draw radius gizmo
 		}
 	}
 }
