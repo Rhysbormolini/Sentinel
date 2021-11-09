9e9c697fe7205fae443b7d8b96b7e4a65f8e1233
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2021-11-09 21:42:07 +0800

    Lock On stuff failed, moving onto redesigning code for abilities

--- Sentinel Titans War/Assets/Sentinel/Scripts/ProjectileTrigger.cs
+++ Sentinel Titans War/Assets/Sentinel/Scripts/ProjectileTrigger.cs
@@ -2,48 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileTrigger : MonoBehaviour
+namespace Gamekit3D
 {
-
-    public GameObject tornadoProjectile;    // this is a reference to your projectile prefab
-    public Transform tornadoSpawnTransform; // this is a reference to the transform where the prefab will spawn
-
-    [SerializeField]
-    float tornadoAbilityCoolDownTime;
-    float tornadoAbilityCoolDown;
-
-    public GameObject lightningBallProjectile;    // this is a reference to your projectile prefab
-    public Transform lightningBallSpawnTransform; // this is a reference to the transform where the prefab will spawn
-
-    [SerializeField]
-    float lightningBallAbilityCoolDownTime;
-    float lightningBallAbilityCoolDown;
-
-    //public Animator m_Animator;
-
-    private void Update()
+    public class ProjectileTrigger : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.Q) && tornadoAbilityCoolDown <= 0)
-        {
-            //m_Animator.Play("TornadoAnim");
-            Instantiate(tornadoProjectile, tornadoSpawnTransform.position, tornadoSpawnTransform.rotation);
-            tornadoAbilityCoolDown = tornadoAbilityCoolDownTime;
-        }
+        [Header("Tornado")]
+        public GameObject tornadoProjectile;    // this is a reference to your projectile prefab
+        public Transform tornadoSpawnTransform; // this is a reference to the transform where the prefab will spawn
+        [SerializeField] float tornadoAbilityCoolDownTime;
+        [SerializeField] float tornadoAbilityCoolDown;
+        public int TornadoDamage;
 
-        if (tornadoAbilityCoolDown > 0)
-        {
-            tornadoAbilityCoolDown -= Time.deltaTime;
-        }
 
-        if (Input.GetKeyDown(KeyCode.E) && lightningBallAbilityCoolDown <= 0)
-        {
-            Instantiate(lightningBallProjectile, lightningBallSpawnTransform.position, lightningBallSpawnTransform.rotation);
-            lightningBallAbilityCoolDown = lightningBallAbilityCoolDownTime;
-        }
+        [Header("Chain Lightning")]
+        public GameObject lightningBallProjectile;    // this is a reference to your projectile prefab
+        public Transform lightningBallSpawnTransform; // this is a reference to the transform where the prefab will spawn
+        [SerializeField] float lightningBallAbilityCoolDownTime;
+        [SerializeField] float lightningBallAbilityCoolDown;
+        public int lightningDamage;
 
-        if (lightningBallAbilityCoolDown > 0)
+        
+
+        private void Awake()
         {
-            lightningBallAbilityCoolDown -= Time.deltaTime;
+            tornadoProjectile.gameObject.GetComponent<ContactDamager>();
+    
+    }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q) && tornadoAbilityCoolDown <= 0)
+            {
+                //m_Animator.Play("TornadoAnim");
+                Instantiate(tornadoProjectile, tornadoSpawnTransform.position, tornadoSpawnTransform.rotation);
+                tornadoAbilityCoolDown = tornadoAbilityCoolDownTime;
+            }
+
+            if (tornadoAbilityCoolDown > 0)
+            {
+                tornadoAbilityCoolDown -= Time.deltaTime;
+            }
+
+            if (Input.GetKeyDown(KeyCode.E) && lightningBallAbilityCoolDown <= 0)
+            {
+                Instantiate(lightningBallProjectile, lightningBallSpawnTransform.position, lightningBallSpawnTransform.rotation);
+                lightningBallAbilityCoolDown = lightningBallAbilityCoolDownTime;
+            }
+
+            if (lightningBallAbilityCoolDown > 0)
+            {
+                lightningBallAbilityCoolDown -= Time.deltaTime;
+            }
         }
     }
 }
