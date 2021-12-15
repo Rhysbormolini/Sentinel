07f053a799185eae1440b5044aff0051385f34df
Rhysbormolini <61007759+Rhysbormolini@users.noreply.github.com>
2021-12-15 17:06:30 +0800

    Revert "Revert "Finished Spikes""
    This reverts commit b86a983e098fad878febde4bd9e0c95eeadb5c66.

--- Sentinel Titans War/Assets/SpikeControllerEditor.cs
+++ Sentinel Titans War/Assets/SpikeControllerEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+#if UNITY_EDITOR
 [CustomEditor(typeof(SpikeController))]
 public class SpikeControllerEditor : Editor
 {
@@ -17,3 +18,4 @@ public class SpikeControllerEditor : Editor
 		}
 	}
 }
+#endif
